3e8ba3c396dcf10f8601b3342e08ab11e98f1afb
DC <43692686+excaliburDC@users.noreply.github.com>
2020-11-06 13:15:44 +0530

    added player models

---
+++ Assets/Scripts/ThirdPersonController/PlayerInput.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TPP
+{
+    public class PlayerInput : MonoBehaviour
+    {
+        public Action<Vector2> OnMovementInput;
+        public Action<Vector3> OnMovementDirectionInput;
+
+        private Camera cam;
+
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            cam = Camera.main;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            GetMovementInput();
+            GetMovementDirectionInput();
+        }
+
+        private void GetMovementDirectionInput()
+        {
+            
+        }
+
+        private void GetMovementInput()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Assets/Scripts/ThirdPersonController/TPP_Controller.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(CapsuleCollider))]
+[RequireComponent(typeof(Animator))]
+public class TPP_Controller : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
