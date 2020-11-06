ef60230a9c12081a7bad3e12c9b743a0cd82baba
DC <43692686+excaliburDC@users.noreply.github.com>
2020-11-06 20:28:00 +0530

    added groundcheck for tpp

--- Assets/Scripts/ThirdPersonController/PlayerInput.cs
+++ Assets/Scripts/ThirdPersonController/PlayerInput.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 
 namespace TPP
 {
-    public class PlayerInput : MonoBehaviour
+    public class PlayerInput : MonoBehaviour, IInput
     {
-        public Action<Vector2> OnMovementInput;
-        public Action<Vector3> OnMovementDirectionInput;
+        public Action<Vector2> OnMovementInput { get; set; }
+        public Action<Vector3> OnMovementDirectionInput { get; set; }
 
         private Camera cam;
 
@@ -29,12 +29,19 @@ namespace TPP
 
         private void GetMovementDirectionInput()
         {
-            
+            Vector3 cameraForwardDirection = cam.transform.forward;
+            Debug.DrawRay(cam.transform.position, cameraForwardDirection * 10, Color.red);
+
+            Vector3 directionToMoveIn = Vector3.Scale(cameraForwardDirection, (Vector3.right + Vector3.forward));
+            Debug.DrawRay(cam.transform.position, directionToMoveIn * 10, Color.blue);
+
+            OnMovementDirectionInput?.Invoke(directionToMoveIn);
         }
 
         private void GetMovementInput()
         {
-            throw new NotImplementedException();
+            Vector2 input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+            OnMovementInput?.Invoke(input);
         }
     }
 }
--- Assets/Scripts/ThirdPersonController/TPP_Controller.cs
+++ Assets/Scripts/ThirdPersonController/TPP_Controller.cs
@@ -2,22 +2,71 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
-[RequireComponent(typeof(Rigidbody))]
-[RequireComponent(typeof(CapsuleCollider))]
-[RequireComponent(typeof(Animator))]
-public class TPP_Controller : MonoBehaviour
+namespace TPP
 {
-
-    // Start is called before the first frame update
-    void Start()
+    [RequireComponent(typeof(Rigidbody))]
+    [RequireComponent(typeof(CapsuleCollider))]
+    [RequireComponent(typeof(Animator))]
+    public class TPP_Controller : MonoBehaviour
     {
-        
-    }
+        [SerializeField] private float moveSpeed =2.0f;
+        [SerializeField] private float turnSpeed = 0.2f;
+        [SerializeField] private float groundDist = 0.2f;
+        [SerializeField] private LayerMask groundMask;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private IInput input;
+        private Rigidbody rb;
+        private CapsuleCollider capsule;
+        private Animator animator;
+
+        private Vector3 m_CapsuleCentre;
+
+        //private bool isGrounded;
+        private bool isCrouching;
+
+        private float m_CapsuleHeight;
+
+
+        private void Awake()
+        {
+            input = GetComponent<IInput>();
+            rb = GetComponent<Rigidbody>();
+            animator = GetComponent<Animator>();
+            capsule = GetComponent<CapsuleCollider>();
+            m_CapsuleHeight = capsule.height;
+            m_CapsuleCentre = capsule.center;
+
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+            input.OnMovementInput += HandleMovement;
+            input.OnMovementDirectionInput += (input) => { Debug.Log("Direction " + input); };
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        void HandleMovement(Vector2 input)
+        {
+            if(IsGrounded())
+            {
+                Debug.LogError("Grounded");
+            }
+        }
+
+        private bool IsGrounded()
+        {
+           return Physics.CheckSphere(transform.position,groundDist,groundMask, QueryTriggerInteraction.Ignore);
+
+
+        }
+
+
     }
 }
