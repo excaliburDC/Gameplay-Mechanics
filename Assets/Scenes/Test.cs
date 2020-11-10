8f0dc93aabb544d89e2c11beb29498aa32fba29c
DC <43692686+excaliburDC@users.noreply.github.com>
2020-11-10 12:37:21 +0530

    updated player movement

---
+++ Assets/Scenes/Test.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log( Vector3.Slerp(Vector3.zero, Vector3.one, 0.5f));
+        
+      
+    }
+}
--- Assets/Scripts/ThirdPersonController/TPP_Controller.cs
+++ Assets/Scripts/ThirdPersonController/TPP_Controller.cs
@@ -14,16 +14,19 @@ namespace TPP
         [SerializeField] private float groundDist = 0.2f;
         [SerializeField] private LayerMask groundMask;
 
+
         private IInput input;
         private Rigidbody rb;
         private CapsuleCollider capsule;
         private Animator animator;
 
+        private Vector3 moveVector = Vector3.zero;
         private Vector3 m_CapsuleCentre;
 
         //private bool isGrounded;
         private bool isCrouching;
 
+        private float desiredRotationAngle = 0f;
         private float m_CapsuleHeight;
 
 
@@ -39,32 +42,89 @@ namespace TPP
         }
 
         // Start is called before the first frame update
-        void Start()
+        private void OnEnable()
         {
 
             input.OnMovementInput += HandleMovement;
-            input.OnMovementDirectionInput += (input) => { Debug.Log("Direction " + input); };
+            input.OnMovementDirectionInput += HandleMovementDirection;
         }
 
         // Update is called once per frame
         void Update()
         {
-
+            //if (IsGrounded() && moveVector.magnitude > 0)
+            //{
+            //    RotatePlayer();
+            //}
         }
 
-        void HandleMovement(Vector2 input)
+        private void FixedUpdate()
         {
+            if (IsGrounded() && moveVector.magnitude > 0)
+            {
+                RotatePlayer();
+            }
+
+            rb.MovePosition(rb.position + moveVector.normalized * Time.fixedDeltaTime);
+        }
+
+        private void HandleMovement(Vector2 input)
+        {
+            
             if(IsGrounded())
             {
                 Debug.LogError("Grounded");
+                if (input.y>0)
+                {
+                    moveVector = transform.forward * moveSpeed;
+                }
+
+                else
+                {
+                    moveVector = Vector3.zero;
+                }
+             
+                
+                
+            }
+        }
+
+        private void HandleMovementDirection(Vector3 direction)
+        {
+            desiredRotationAngle = Vector3.Angle(transform.forward, direction);
+            var crossProduct = Vector3.Cross(transform.forward, direction).y;
+
+            if(crossProduct<0)
+            {
+                desiredRotationAngle *= -1;
+            }
+
+        }
+
+        void RotatePlayer()
+        {
+            if(desiredRotationAngle > 10 || desiredRotationAngle < -10) 
+            {
+                Quaternion deltaRotation = Quaternion.Euler(Vector3.up * desiredRotationAngle * turnSpeed * Time.deltaTime);
+                rb.MoveRotation(rb.rotation * deltaRotation);
+
+                //transform.Rotate(Vector3.up * desiredRotationAngle * turnSpeed * Time.deltaTime);
             }
         }
 
         private bool IsGrounded()
         {
+           
            return Physics.CheckSphere(transform.position,groundDist,groundMask, QueryTriggerInteraction.Ignore);
 
 
+        }
+
+        private void OnDisable()
+        {
+
+            input.OnMovementInput -= HandleMovement;
+            input.OnMovementDirectionInput -= HandleMovementDirection;
         }
 
 
